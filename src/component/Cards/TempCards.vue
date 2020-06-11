bc083df4fd419898f8e04d05ce8c8d88dfde0b27
yashdhume <yash_dhume@yahoo.ca>
2020-06-10 23:22:21 -0400

    Temp Cards added for future use

---
+++ src/component/Cards/TempCards.vue
@@ -0,0 +1,22 @@
+<template>
+    <vs-row vs-justify="center">
+        <vs-col type="flex" vs-align="center" vs-justify="center" vs-w="6">
+            <vs-card actionable class="cardx" fixed-height="10%">
+                <div slot="header">
+                    <h3>Something</h3>
+                </div>
+                <div slot="media">
+                    <img src="https://www.universitystudy.ca/wp-content/uploads/2013/08/university-ontario-institute-of-technology-campus-image.jpg">
+                </div>
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                </div>
+            </vs-card>
+        </vs-col>
+    </vs-row>
+</template>
+<script>
+    export default {
+        name: "TempCards"
+    }
+</script>
