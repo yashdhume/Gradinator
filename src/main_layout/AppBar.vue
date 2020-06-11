6a4155dbaf3a3eff6bc743d5a19d2103dc78433d
yashdhume <yash_dhume@yahoo.ca>
2020-06-10 23:19:06 -0400

    Implemented main skeleton

---
+++ src/main_layout/AppBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="gridx">
+        <v-app-bar clipped-right color="transparent" flat id="AppBar">
+            <v-app-bar-nav-icon @click.stop="activeDrawer">
+                <v-icon>menu</v-icon>
+            </v-app-bar-nav-icon>
+            <v-toolbar-title>Gradinator</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-app-bar>
+
+    </header>
+
+
+</template>
+
+<script>
+    export default {
+        name: "AppBar",
+        methods: {
+            activeDrawer() {
+                this.$store.commit('IsDrawerActive', true);
+            }
+
+        }
+    }
+</script>
