b9d0ab43e5eac229c1f7f9cf265731bc8fdcc5a0
yashdhume <yash_dhume@yahoo.ca>
2020-06-11 23:38:00 -0400

    DocumentCard redesign and removed dummy data

--- src/component/Cards/DocumentCard.vue
+++ src/component/Cards/DocumentCard.vue
@@ -1,67 +1,67 @@
 <template>
-    <vs-card>
-        <section>
-            <h3 class="card-title d-flex">
-                Content List
-                <vs-button
-                        @click="isPopupActive=true"
-                        :color="isGet ? 'success' : 'primary'"
-                        type="flat"
-                        class="ml-auto p-0"
-                >
-                    {{isGet ? 'Get' : 'Post'}}
-                </vs-button>
-            </h3>
-            <p class="card-subtitle">
-                You can add custom content to the item. It will be pushed to the right side.
-            </p>
-        </section>
-        <vs-popup
-                style="color:rgb(255,255,255)"
-                background-color="rgba(255,255,255,.6)"
-                :background-color-popup="this.isGet ?'rgb(23, 201, 100)' :'#5b3cc4'" :title="this.isGet ? 'Get' : 'Post'" :active.sync="isPopupActive">
-            <p>
-                {{isGet ? 'Some Details for Get' : 'Some Details for Post'}}
+    <div>
+        <vs-row vs-justify="center">
+            <vs-col type="flex" vs-align="center" vs-justify="center" vs-w="8">
+                <div v-for="x in endpoints" :key="x">
+                    <vs-card actionable>
+                        <div slot="header">
+                            <h3>{{x.path}}</h3>
+                            <vs-row vs-justify="flex-end">
+                                <vs-chip :color="x.httpMethod==='POST' ? 'primary': 'success'">
+                                    {{x.httpMethod}}
+                                </vs-chip>
+                            </vs-row>
+                        </div>
+                        <vs-col>
+                            <vs-row  vs-justify="space-around">
+                                <div>
+                                    <vs-list>
+                                        <vs-list-header title="Inputs"></vs-list-header>
+                                        <div v-for="i in x.inputs" :key="i">
+                                            <vs-list-item
+                                                    :title="i.name"
+                                                    :subtitle="i.description"
+                                            ></vs-list-item>
+                                        </div>
+                                    </vs-list>
+                                </div>
+                                <div>
+                                    <vs-list>
+                                        <vs-list-header title="Outputs"></vs-list-header>
+                                        <div v-for="i in x.outputs" :key="i">
+                                            <vs-list-item
+                                                    :title="i.name"
+                                                    :subtitle="i.description"
+                                            ></vs-list-item>
+                                        </div>
+                                    </vs-list>
+                                </div>
+                            </vs-row>
+                        </vs-col>
+                        <div slot="footer">
+                            <vs-row vs-justify="flex-end">
 
-            </p>
-        </vs-popup>
-        <vs-col vs-align="center"
-                vs-justify="space-around" vs-type="flex">
-            <v-container>
-                    <vs-list>
-                        <vs-list-header title="Inputs"></vs-list-header>
-                        <vs-list-item title="Snickerdoodle" subtitle="An excellent companion"></vs-list-item>
+                            </vs-row>
+                        </div>
 
-                    </vs-list>
-                </v-container>
-                <v-container>
-                    <vs-list>
-                        <vs-list-header title="Outputs"></vs-list-header>
-                        <vs-list-item
-                                title="Enid's"
-                                subtitle="At night a bar, during the day a delicious brunch spot."
-                        >
-                            </vs-list-item>
-                        <vs-list-item title="Veronika Ossi" subtitle="Has not watched anything recently">
-                            </vs-list-item>
-                    </vs-list>
-                </v-container>
-        </vs-col>
-
-    </vs-card>
+                    </vs-card>
+                </div>
+            </vs-col>
+        </vs-row>
+    </div>
 </template>
 
 <script>
     export default {
         name: "DocumentCard",
-        data:()=>({
+        props: {
+            endpoints: Array
+        },
+        data: () => ({
             isPopupActive: false,
-            isGet: false,
+            isGet: true,
 
         }),
     }
 </script>
 
-<style scoped>
-
-</style>
