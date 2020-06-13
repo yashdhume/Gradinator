2d1fb506f1a9e6bc3bf72b0ac225ddd5f8071418
Vincent G <34199896+VincentGalloro@users.noreply.github.com>
2020-06-13 13:41:29 -0400

    Added endpoint categories
    Added all endpoint categories to documentation page. Added headers for category.

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -1,7 +1,24 @@
 <template>
     <vs-row vs-justify="center">
         <vs-col type="flex" vs-justify="center" vs-align="center" vs-lg="12" vs-xs="12">
-            <DocumentCard v-bind:endpoints="documentationData.data.endpoints[0].courseEndpoints"></DocumentCard>
+
+            <!-- TODO: YASH: Make this not hardcoded -->
+
+            <vs-row vs-justify="center" vs-align="center">
+                <h2>Course Endpoints</h2>
+            </vs-row>
+            <DocumentCard v-bind:endpoints="documentationData.data.endpoints['courseEndpoints']"></DocumentCard>
+
+            <vs-row vs-justify="center" vs-align="center">
+                <h2>Assessment Endpoints</h2>
+            </vs-row>
+            <DocumentCard v-bind:endpoints="documentationData.data.endpoints['assessmentEndpoints']"></DocumentCard>
+
+            <vs-row vs-justify="center" vs-align="center">
+                <h2>Users Endpoints</h2>
+            </vs-row>
+            <DocumentCard v-bind:endpoints="documentationData.data.endpoints['userEndpoints']"></DocumentCard>
+
            </vs-col>
     </vs-row>
 
