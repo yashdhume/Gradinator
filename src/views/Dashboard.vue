7d1fc7f3f81f5130aaed1c8fca36c7e4209477f7
yashdhume <yash_dhume@yahoo.ca>
2020-06-15 21:10:13 -0400

    Code cleanup

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,20 +2,16 @@
     <v-app>
         <IndividualGradeCards/>
         <LineGraphCard/>
-        <!--        <BarChartCard/>-->
         <TempCards/>
-        <DocumentCard/>
-
     </v-app>
 </template>
 <script>
     import IndividualGradeCards from "../components/Cards/IndividualGradeCards";
     import LineGraphCard from "../components/Cards/Graphs/LineGraphCard";
     import TempCards from "../component/Cards/TempCards";
-    import DocumentCard from "../components/Cards/DocumentCards";
-    // import BarChartCard from "../components/BarChartCard";
+    import CourseCard from "../component/Cards/CourseCard";
     export default {
         name: "Dashboard",
-        components: {DocumentCard, TempCards, IndividualGradeCards, LineGraphCard}
+        components: {CourseCard,TempCards, IndividualGradeCards, LineGraphCard}
     }
 </script>
