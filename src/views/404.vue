e38fce672234ca497d1e196bd713e163c187729d
yashdhume <yash_dhume@yahoo.ca>
2020-06-18 19:50:30 -0400

    404 page added

---
+++ src/views/404.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="centerx">
+        <lottie
+                :options="errorOptions"
+                :height="500"
+                :width="500"/>
+    </div>
+</template>
+
+<script>
+    import Lottie from "vue-lottie";
+    import error from "./../animations/404";
+
+    export default {
+        name: "404",
+        components: {
+            Lottie
+        },
+        data() {
+            return {
+                activeIndex: 0,
+                errorOptions: {animationData: error, loop: true},
+                animationSpeed: 1
+            };
+        }
+    };
+</script>
+
