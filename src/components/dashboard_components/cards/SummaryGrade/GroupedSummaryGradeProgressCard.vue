5cf6c0f527dbcd2e56156604e16efe3c9cc4dfd8
yashdhume <yash_dhume@yahoo.ca>
2020-07-11 16:51:51 -0400

    Fixed bug

--- src/components/dashboard_components/cards/SummaryGrade/GroupedSummaryGradeProgressCard.vue
+++ src/components/dashboard_components/cards/SummaryGrade/GroupedSummaryGradeProgressCard.vue
@@ -1,9 +1,8 @@
 <template>
     <vs-row vs-align="center"
             vs-justify="space-around" vs-type="flex">
-        <div :key="item" v-for="(item,index) in this.enrolledCourses.data.courses">
+        <div :key="item" v-for="(item,index) in this.enrolledCourses.courses">
             <vs-col>
-
                 <SummaryGradeProgressCard
                         v-bind:prof="item.course.professor"
                         v-bind:roomNumber="item.course.room"
@@ -18,24 +17,32 @@
 
 <script>
     import SummaryGradeProgressCard from "./SummaryGradeProgressCard";
+    import {getEnrolledCourses} from "../../../../api/api";
     import {mapState} from "vuex";
 
     export default {
         name: "GroupedSummaryGradeProgressCard",
         components: {SummaryGradeProgressCard},
+        computed: mapState([
+            'token'
+        ]),
+        mounted() {
+            this.reload();
+        },
         data: () => ({
+            enrolledCourses: [],
             dummyGrades:[{currentGrade: 0.75, minimumGrade: 0.6, maximumGrade: 0.87},
+                {currentGrade: 0.35, minimumGrade: 0.2, maximumGrade: 0.64},
+                {currentGrade: 0.80, minimumGrade: 0.75, maximumGrade: 0.96},
+                {currentGrade: 0.45, minimumGrade: 0.35, maximumGrade: 0.52},
                 {currentGrade: 0.35, minimumGrade: 0.2, maximumGrade: 0.64},
                 {currentGrade: 0.80, minimumGrade: 0.75, maximumGrade: 0.96},
                 {currentGrade: 0.45, minimumGrade: 0.35, maximumGrade: 0.52}]
         }),
-        mounted() {
-            this.$store.dispatch('loadEnrolledCourses')
-        },
-        computed:{
-            ...mapState([
-                'enrolledCourses'
-            ])
+        methods: {
+            reload: function(){
+                getEnrolledCourses(this.token).then(r=> this.enrolledCourses = r)
+            }
         },
 
     }
