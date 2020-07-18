6391ec6731cb703cb548c99de1231d28674ba0ae
yashdhume <yash_dhume@yahoo.ca>
2020-07-18 13:53:26 -0400

    changed router # issue

--- src/router.js
+++ src/router.js
@@ -3,7 +3,8 @@ import Router from 'vue-router'
 
 Vue.use(Router);
 export default new Router({
-    mode: 'hash',
+    mode: 'history',
+    base: process.env.BASE_URL,
     routes: [
         {
             path: '',
